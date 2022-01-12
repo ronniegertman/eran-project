fe187ea48df09f83fa8c8e804e6b32b983c7b55a
Ronnie Gertman <ronniegert2707@gmail.com>
2022-01-12 16:20:29 +0200

    handling errors

--- public/js/feeling.js
+++ public/js/feeling.js
@@ -18,26 +18,7 @@ function handleButton(button){
     }
     const Myelement = document.getElementById('7')
     Myelement.setAttribute('value', JSON.stringify(toSend))
-    console.log(toSend)
-    console.log(count)
-
 }
-
-
-
-
-console.log(Myelement)
-let toSend = {
-    cry: count[0]% 2 === 1,
-    depressed: count[1] % 2 === 1,
-    happy: count[2] % 2 === 1,
-    stressed: count[3] % 2 === 1, 
-    disappointed: count[4] % 2 === 1,
-    calm: count[5] % 2 === 1
-}
-
-
-console.log(Myelement)
 
 
 
