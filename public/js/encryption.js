b6d7362f41c5ca945a49e61392280b557424377b
Ronnie Gertman <ronniegert2707@gmail.com>
2022-04-06 11:33:07 +0300

    editing login page

--- public/js/encryption.js
+++ public/js/encryption.js
@@ -1,4 +1,22 @@
 // const res = require("express/lib/response");
+async function encrypt(keyPair, message) {
+  let enc = new TextEncoder();
+  let text = enc.encode(message);
+  const encrypted = await crypto.subtle.encrypt("RSA-OAEP", keyPair.publicKey, text);
+  return btoa(String.fromCharCode(...new Uint8Array(encrypted)))
+}
+
+async function decrypt(keyPair, encrypted) {
+  try{
+    const buffer = Uint8Array.from(atob(encrypted), c => c.charCodeAt(0))
+    const decrypted = await crypto.subtle.decrypt("RSA-OAEP", keyPair.privateKey, buffer);
+    let dec = new TextDecoder();
+    return(dec.decode(decrypted));
+  } catch(e){
+    console.error(e )
+  }
+
+}
 
 async function run(){
     let keyPair = await window.crypto.subtle.generateKey(
@@ -11,6 +29,15 @@ async function run(){
         true,
         ["encrypt", "decrypt"]
       );
+      
+
+    
+      const enc = await encrypt(keyPair, 'hello world');
+      const dec = await decrypt(keyPair, enc)
+      console.log(enc)
+      console.log(dec)
+      // console.log(btoa(String.fromCharCode(...new Uint8Array(enc))))
+
 
       const result = {
           private: await crypto.subtle.exportKey('jwk', keyPair.privateKey),
@@ -20,12 +47,14 @@ async function run(){
 }
 
 run().then(res => {
+    
     console.log('here')
-    document.getElementById('privateKey').value = JSON.stringify(res.private)
-    document.getElementById('publicKey').value = JSON.stringify(res.public)
-    const text = 'hello world'
-    const encrypted = crypto.subtle.encrypt("RSA-OAEP", res.public, text).then(result => console.log(result))
+    // document.getElementById('privateKey').value = JSON.stringify(res.private)
+    // document.getElementById('publicKey').value = JSON.stringify(res.public)
+    
 })
+
+
 
 
 
